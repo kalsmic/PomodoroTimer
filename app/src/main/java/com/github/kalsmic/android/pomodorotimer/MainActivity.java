c29182ef59c61b4bf28df9231894ea58f2613768
Arthur Kalule <kalulearthur@gmail.com>
2021-08-01 07:50:16 +0300

    add dummy menu to be replace later

--- app/src/main/java/com/github/kalsmic/android/pomodorotimer/MainActivity.java
+++ app/src/main/java/com/github/kalsmic/android/pomodorotimer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.github.kalsmic.android.pomodorotimer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.menuSounds:
+                Toast.makeText(this, "Menu Sounds Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menuAbout:
+                Toast.makeText(this, "Menu About Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menuShare:
+                Toast.makeText(this, "Menu Share Clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
