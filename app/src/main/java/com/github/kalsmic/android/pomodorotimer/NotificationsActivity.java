22a5f4a108ba63f89cea79fcd02f8d78520caa40
Arthur Kalule <kalulearthur@gmail.com>
2021-08-02 16:00:51 +0300

    add playing sound on select

--- app/src/main/java/com/github/kalsmic/android/pomodorotimer/NotificationsActivity.java
+++ app/src/main/java/com/github/kalsmic/android/pomodorotimer/NotificationsActivity.java
@@ -2,6 +2,7 @@ package com.github.kalsmic.android.pomodorotimer;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class NotificationsActivity extends AppCompatActivity {
 
     int currentNotificationId = 0;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +33,7 @@ public class NotificationsActivity extends AppCompatActivity {
             currentNotificationId = soundPreferences.getInt("soundId", 0);
         }
 
-        ListView listViewSounds = (ListView) findViewById(R.id.listView_sounds);
+        ListView listViewSounds = findViewById(R.id.listView_sounds);
 
         // populate list view with sounds
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -47,11 +49,20 @@ public class NotificationsActivity extends AppCompatActivity {
         listViewSounds.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Always release mediaPlay Object if it exists
+                if (mediaPlayer != null) {
+                    mediaPlayer.release();
+                }
+                // force release object reference
+                mediaPlayer = null;
+                // create new MediaPlayer object for selected sound
+                getMediaPlayer(position);
+                mediaPlayer.start();
                 currentNotificationId = position;
             }
         });
 
-        Button saveSoundButton = (Button) findViewById(R.id.button_save_sound);
+        Button saveSoundButton = findViewById(R.id.button_save_sound);
 
         saveSoundButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,5 +79,23 @@ public class NotificationsActivity extends AppCompatActivity {
 
 
     }
+
+    /**
+     * This method creates a media sound object.
+     *
+     * @param soundId is the reference to the sound
+     * @return MediaPlayer object for sound with id soundId
+     */
+    public MediaPlayer getMediaPlayer(int soundId) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), Sound.getSound(soundId));
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mediaPlayer.release();
+            }
+        });
+        return mediaPlayer;
+    }
+
 
 }
