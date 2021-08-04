27751171716406936f5511016213c446db20a741
Arthur Kalule <kalulearthur@gmail.com>
2021-08-04 07:21:22 +0300

    remove unused import

--- app/src/main/java/com/github/kalsmic/android/pomodorotimer/HomeActivity.java
+++ app/src/main/java/com/github/kalsmic/android/pomodorotimer/HomeActivity.java
@@ -1,7 +1,6 @@
 package com.github.kalsmic.android.pomodorotimer;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
